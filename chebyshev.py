06334b3fcbe804f2d4526787a3a13b73a9ed3256
nakib <nakib@nakib-F82Q.(none)>
2012-06-24 18:54:46 -0400

    integration function is problemtic

--- chebyshev.py
+++ chebyshev.py
@@ -1,18 +1,26 @@
-from numpy import arange, outer, cos, pi, arccos, dot, linspace, exp
+from numpy import arange, outer, sin, cos, pi, arccos, dot, linspace, exp
 from scipy.linalg import solve
-from pylab import plot, legend, grid, show
+import sympy, sympy.abc
+from pylab import zeros, plot, legend, grid, show
 
-a = 0.0
-b = 0.1
-N = 10 #number of points on grid
+a = -1.0
+b = 1.0
+N = 20 #number of points on grid
 
 # test function
 def testFunc(x):
-    return exp(-10*x*x)
+    return sin(2*x)
 
-# transform any interval to [a,b]
+# transform [-1,1] to [a,b]
 def affineTransform(x, a, b):
     return (a+b)/2.0 + ((b-a)/2.0)*x
+
+# integrate function analytically in [-1,1]
+def integrate(C, a, b):
+    I = 0.0
+    for i in range(0, N):
+        I += C[i]*sympy.integrate(sympy.cos(i*sympy.acos(sympy.abc.x))/(pow(2.0, i-1)), (sympy.abc.x, a, b))
+    print(I)
 
 # Form a Chebyshev mesh and perform the Chebyshev interpolation
 # N points on the mesh
@@ -20,13 +28,19 @@ def chebInterpol(N, a, b):
     i = arange(N)
     # Chebyshev nodes on [-1,1]
     x = -cos(pi*(2.0*i + 1.0)/(2.0*(N-1) + 2.0)) 
+    
     # form the (normalized) Chebyshev-Vandermonde matrix
     TNx = cos(outer(arccos(x), i))/(pow(2.0, i-1))
     
     # scale to physical interval [a,b]
-    x = affineTransform(x, a, b)
+    x = affineTransform(x, a, b)    
+    
     f = testFunc(x)
+    # C holds the coefficient of each Cheb harmonic
     C = solve(TNx, f)
+
+    # integrate the interpolation function over [a,b]
+    integrate(C, a, b)
     
     # form a uniform grid on [-1,1] to test interpolation
     xnew = linspace(-1.0, 1.0, N)
@@ -39,7 +53,7 @@ def chebInterpol(N, a, b):
     
     # plot the Chebyshev nodes and the interpolation
     plot(x, f, 'r.', label = "func(Chebyshev nodes)")
-    plot(xnew, fnew, 'go', label = "Chebyshev interpolation")
+    plot(xnew, fnew, 'g.', label = "Chebyshev interpolation")
     legend(loc="best")
     grid(True)
     show()
